b32c76069c2f18b9d808a5fe565509ae411446d9
Alexandru Grapa <alexandru22valentin@gmail.com>
2021-07-07 17:18:55 +0100

    Restyle the rating stars of the range input to fill in and out

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,21 @@
 // CREDITS: https://css-tricks.com/value-bubbles-for-range-inputs/
 //
 var rangeSlider = function () {
-    var slider = $('.range-slider'),
-        range = $('.range-slider__range'),
-        value = $('.range-slider__value');
+    var range = $('.range-slider__range'),
+        value = $('.range-slider__value'),
+        stars = $('.stars-container');
 
-    slider.each(function () {
+    value.each(function () {
+        var value = $(range).attr('value');
+        var width = 100 / 5 * value;
+        $(this).html(value);
+        $(stars).width(width + '%');
+    });
 
-        value.each(function () {
-            var value = $(this).prev().attr('value');
-            $(this).html(value);
-        });
-
-        range.on('input', function () {
-            $(this).next(value).html(this.value);
-        });
+    range.on('input', function () {
+        var width = 100 / 5 * this.value;
+        $(value).html(this.value);
+        $(stars).width(width + '%');
     });
 };
 
